498065c4230ab219b19deae4de229e9830828b99
snolly <kassnollygv4462>
2018-01-11 02:01:46 +0300

    add rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,88 +9,111 @@ declare var $;
 })
 
 export class AppComponent {
-
+  firstLoad:boolean = true;
   title = 'app works!';
   MenuActiveTab:number = 0;
   constructor(){
 
-     
-$(document).ready(function(){
-	$("#menu").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-    var id  = $(this).attr('href')
-    ,
-  
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		console.log(id, top);
-    //анимируем переход на расстояние - top за 1500 мс
-    if(id=="#what_for_you")top+=400;
-		$('body,html').animate({scrollTop: top}, 1400);
-	});
-});
+     this.addScroll();
 
 
-  var menu_selector = ".icon-bar"; 
-  
-  function onScroll(){
-    var scroll_top = $(document).scrollTop();
-    $(menu_selector  + " a").each(function(){
-      var hash = $(this).attr("href");
-      
-      var target = $(hash);
-     // console.log(`!`,$(this),hash,$(this).attr("href"),target);
-
-      console.log(target, target.position().top);
-      if (target.position().top <= scroll_top +150 && target.position().top + target.outerHeight() > scroll_top) {
-      //  $(menu_selector + " a span.active").addClass("active");
-      // $(a span.active).addClass("active");
-      $("a span.active").removeClass("active");
-       console.log(`this = `, $(this).context.hash);
-        var nn=$(this).context.hash;
-        $("a[href^="+nn+"] span").addClass("active");
-      } else {
-      //  $("a span.active").removeClass("active");
-      }
-    });
   }
 
-$(document).ready(function () {
-	$(document).on("scroll", onScroll);
-	$("a[href^=#]").click(function(e){
-		e.preventDefault();
-		$(document).off("scroll");
-		$(menu_selector + " a span.active").removeClass("active");
-		$(this).addClass("active");
-		var hash = $(this).attr("href");
-		var target = $(hash);
-		$("html, body").animate({
-		    scrollTop: target.offset().top+10
-		}, 500, function(){
-			window.location.hash = hash;
-			$(document).on("scroll", onScroll);
-		});
-	});
-});
 
+  addScroll(){
+    $(document).ready(function(){
+      $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+    
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href')
+        ,
+      
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+          top = $(id).offset().top;
+        console.log(id, top);
+        //анимируем переход на расстояние - top за 1500 мс
+        if(id=="#what_for_you")top+=400;
+        $('body,html').animate({scrollTop: top}, 1400);
+      });
+    });
 
+    
+    
+    
+      var menu_selector = ".icon-bar"; 
+      
+      function onScroll(){
+        var scroll_top = $(document).scrollTop();
+        $(menu_selector  + " a").each(function(){
+          var hash = $(this).attr("href");
+          
+          var target = $(hash);
+         // console.log(`!`,$(this),hash,$(this).attr("href"),target);
+    
+          console.log(target, target.position().top);
+          if (target.position().top <= scroll_top +150 && target.position().top + target.outerHeight() > scroll_top) {
+          //  $(menu_selector + " a span.active").addClass("active");
+          // $(a span.active).addClass("active");
+          $("a span.active").removeClass("active");
+           console.log(`this = `, $(this).context.hash);
+            var nn=$(this).context.hash;
+            
+            $("a[href^="+nn+"] span").addClass("active");
+
+            if(nn=="#what_for_you") {
+              var count = 1;
+              setInterval(()=>{
+                
+              let elem = document.getElementById("myBar"+count); 
+              let elemPr = document.getElementById("myPrProc"+count); 
+             
+              let width = $(elem).width();
+               setInterval(()=>{
+                {
+                  if (width < 100)  {
+                      width++; 
+                      elem.style.width = width + '%';  
+                      $(elemPr).text(width+"%"); 
+                  }
+                  
+              }
+              
+            }, 10);
+            if(count<=5)count++;
+            },800)
+              
+            }
+          } else {
+          //  $("a span.active").removeClass("active");
+          }
+        });
+      }
+    
+    $(document).ready(function () {
+      $(document).on("scroll", onScroll);
+      $("a[href^=#]").click(function(e){
+        e.preventDefault();
+        $(document).off("scroll");
+        $(menu_selector + " a span.active").removeClass("active");
+        $(this).addClass("active");
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        $("html, body").animate({
+            scrollTop: target.offset().top+10
+        }, 500, function(){
+          window.location.hash = hash;
+          $(document).on("scroll", onScroll);
+        });
+      });
+    });
+    
   }
 
 
    move() {
-    let elem = document.getElementById("myBar"); 
-    let width = 1;
-     setInterval(()=>{
-      {
-        if (width < 100)  {
-            width++; 
-            elem.style.width = width + '%'; 
-        }
-    }
-    }, 10);
+   
   }
 
 
